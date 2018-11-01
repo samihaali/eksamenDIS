cf81c48af04067ea23803c8c81a3f09fa84259a2
samiha <saal17ah@student.cbs.dk>
2018-11-01 12:47:08 +0100

    Cache built and used

--- src/main/java/cache/OrderCache.java
+++ src/main/java/cache/OrderCache.java
@@ -1,6 +1,49 @@
 package cache;
 
-//TODO: Build this cache and use it.
+import controllers.OrderController;
+import controllers.ProductController;
+import model.Order;
+import model.Product;
+import utils.Config;
+
+import java.util.ArrayList;
+
+//TODO: Build this cache and use it. (FIX)
 public class OrderCache {
 
+    // List of orders
+    private ArrayList<Order> orders;
+
+    // Time cache should live
+    private long ttl;
+
+    // Sets when the cache has been created
+    private long created;
+
+    public OrderCache() {
+        this.ttl = Config.getOrderTtl();
+    }
+
+    public ArrayList<Order> getOrders(Boolean forceUpdate) {
+
+        // If we whis to clear cache, we can set force update.
+        // Otherwise we look at the age of the cache and figure out if we should update.
+        // If the list is empty we also check for new orders
+        if (forceUpdate
+                || ((this.created + this.ttl) >= (System.currentTimeMillis() / 1000L))
+                || this.orders == null) {
+
+            // Get orders from controller, since we wish to update.
+            ArrayList<Order> orders = OrderController.getOrders();
+
+            // Set orders for the instance and set created timestamp
+            this.orders= orders;
+            this.created = System.currentTimeMillis() / 1000L;
+        }
+
+        // Return the documents
+        return this.orders;
+    }
 }
+
+
--- src/main/java/com/cbsexam/OrderEndpoints.java
+++ src/main/java/com/cbsexam/OrderEndpoints.java
@@ -1,5 +1,6 @@
 package com.cbsexam;
 
+import cache.OrderCache;
 import com.google.gson.Gson;
 import controllers.OrderController;
 import java.util.ArrayList;
@@ -43,7 +44,9 @@ public class OrderEndpoints {
   public Response getOrders() {
 
     // Call our controller-layer in order to get the order from the DB
-    ArrayList<Order> orders = OrderController.getOrders();
+    OrderCache orderCache = new OrderCache();
+    ArrayList<Order> orders = orderCache.getOrders(false);
+
 
     // TODO: Add Encryption to JSON (FIX)
     // We convert the java object to json with GSON library imported in Maven
