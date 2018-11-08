dff480bf0dcc5f9de5b9b2685d71ad80f508365a
samiha <saal17ah@student.cbs.dk>
2018-11-08 16:36:34 +0100

    when somehting breaks down

--- src/main/java/com/cbsexam/UserEndpoints.java
+++ src/main/java/com/cbsexam/UserEndpoints.java
@@ -34,7 +34,14 @@ public class UserEndpoints {
 
     // Return the user with the status code 200
     // TODO: What should happen if something breaks down?
-    return Response.status(200).type(MediaType.APPLICATION_JSON_TYPE).entity(json).build();
+
+    if (user !=null){
+      return Response.status(200).type(MediaType.APPLICATION_JSON_TYPE).entity(json).build();
+    }else {
+      return Response.status(400).entity("User not found").build();
+    }
+
+
   }
 
   /**
