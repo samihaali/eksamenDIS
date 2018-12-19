6268752f0190f0356d33e4e48a2dd9be93b30d63
samiha <saal17ah@student.cbs.dk>
2018-12-19 16:27:05 +0100

    XOR explained

--- src/main/java/utils/Encryption.java
+++ src/main/java/utils/Encryption.java
@@ -14,7 +14,19 @@ public final class Encryption {
       // Stringbuilder enables you to play around with strings and make useful stuff
       StringBuilder thisIsEncrypted = new StringBuilder();
 
-      // TODO: This is where the magic of XOR is happening. Are you able to explain what is going on?
+      // TODO: This is where the magic of XOR is happening. Are you able to explain what is going on? (FIX)
+      /**
+       -	For-loppet forsætter med at køre, men stopper når der ikke er flere karakterer i rawString
+       -	RawString er den string, som vi ønsker at kryptere
+       -    For-loppet kører i første omgang med i=0, derefter bliver den karakter der sat ind på i’s til en binære værdi
+       -	Dette samme gøres med karakteren, som er på i’s plads i charArray key.
+       -	Derved har vi 2 binære værdier, som lægges sammen ved brug af XOR
+       -	Der returnenes en ny binære værdi, som bliver omdannet til char, og derefter bliver tilføjet til metoden thisIsEncrypted
+       -    Dette gøres med thisIsEncrypted.append metoden.
+       -	Dette sker indtil der ikke er flere karakter i rawString
+       -	Hvis man oplever at charArray key er mindre end rawString, bruges modulus til at starte charArray key forfra.
+       *
+       */
       for (int i = 0; i < rawString.length(); i++) {
         thisIsEncrypted.append((char) (rawString.charAt(i) ^ key[i % key.length]));
       }
